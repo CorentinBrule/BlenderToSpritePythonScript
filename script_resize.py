01eaada2cf04060e2635a7f834c2b1b251c37d53
Corentin Brulé <corentin.brule@gmail.com>
2015-09-22 02:55:20 +0200

    add sys.argv to chose input and output file and input() fonction to chose new scale

--- script_resize.py
+++ script_resize.py
@@ -1,10 +1,16 @@
+#!/usr/bin/python3.4
+# coding: utf8
+
 from PIL import Image
 import os,sys 
 
 inputPath=sys.argv[1]
 outputPath=sys.argv[2]
 img = Image.open(inputPath)
-img = img.resize((1000,1000))
+print("actual size : ",img.size[0],"x",img.size[1]," pixels.")
+newSizeX=int(input("enter new size x : "))
+newSizeY=int(input("enter new size y : "))
+img = img.resize((newSizeX,newSizeY))
 img.save(outputPath)
 
 
